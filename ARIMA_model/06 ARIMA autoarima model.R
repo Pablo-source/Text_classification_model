ba4fac380e35ca21e82c1e4a35cf5272df73cd09
Pablo Leonrodenas <pablo.leonrodenas@england.nhs.uk>
2023-02-10 16:25:55 +0000

    Update

--- ARIMA_model/06 ARIMA autoarima model.R
+++ ARIMA_model/06 ARIMA autoarima model.R
@@ -10,4 +10,7 @@ md <- auto.arima(train)
 fc <- forecast(md, h = 12)
 
 # Plotting actual vs. fitted and forecasted
-test_forecast(actual = USgas, forecast.obj = fc, test = test)
+test_forecast(actual = AE_major_ts, forecast.obj = fc, test = test)
+
+# It is important to remember that the different plots from TSstudio library are interactive plotly plots. 
+# Users can hover and interrogate the chart and data interactively
